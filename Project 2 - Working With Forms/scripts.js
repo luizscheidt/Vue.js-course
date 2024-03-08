c997ef403c8d540a5f147f32268a9c141642b72d
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-07 21:57:06 -0300

    Project 2 initial code

---
+++ Project 2 - Working With Forms/scripts.js
@@ -0,0 +1,28 @@
+members = [
+  {
+    fname: "John",
+    lname: "Lennon",
+    instrument: "Violão",
+  },
+  {
+    fname: "George",
+    lname: "Harrison",
+    instrument: "Guitarra",
+  },
+];
+
+const handlingForms = {
+  data() {
+    return {
+      members: window.members,
+      newMember: {},
+    };
+  },
+  methods: {
+    addMember: function () {
+      this.members.push(this.newMember);
+    },
+  },
+};
+
+Vue.createApp(handlingForms).mount("#app");
