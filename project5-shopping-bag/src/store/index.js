32a9524833bec4303b169e9c81b8764e90d1a783
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-13 16:19:31 -0300

    Displaying the products from the api

--- project5-shopping-bag/src/store/index.js
+++ project5-shopping-bag/src/store/index.js
@@ -1,8 +1,22 @@
 import { createStore } from "vuex";
+import axios from "axios";
 
 export default createStore({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    products: [],
+  },
+  mutations: {
+    loadProducts(state, response) {
+      state.products = { ...response };
+      console.log(state.products);
+    },
+  },
+  actions: {
+    loadProducts({ commit }) {
+      axios.get("https://fakestoreapi.com/products").then((response) => {
+        commit("loadProducts", response.data);
+      });
+    },
+  },
   modules: {},
 });
