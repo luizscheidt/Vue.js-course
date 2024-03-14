5b289c10ed07a2f373b3631674702007878046b9
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-14 15:27:46 -0300

    Displaying the books from the list and styling the already read books

--- project6-book-list/src/App.vue
+++ project6-book-list/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
-    
+import Books from './components/Books.vue';
+
     let books = [
         {
           id: 1,
@@ -54,24 +55,7 @@
       </div>
    
       <div class="books-container">
-        <div class="books-list">
-          <div class="book">
-            <div class="book-cover">
-              <img src="https://printpress.cmsmasters.net/default/wp-content/uploads/sites/11/2019/05/printpress-product-6-540x861.jpg" />
-    
-              <button>
-                <i class="fa-solid fa-eye"></i>
-                <span>Ainda não li</span
-                >
-              </button>
-            </div>
-            <div class="book-details">
-              <p class="book-author">Daniel Trejo</p>
-              <h3 class="book-title">History of Europe</h3>
-              <p><i class="fa-solid fa-hashtag icon"></i> 0-395-07157-8</p>
-            </div>
-          </div>
-        </div>
+        <Books :books="books" />
       </div>
     </div>
 </template>
---
+++ project6-book-list/src/components/Books.vue
@@ -0,0 +1,27 @@
+<script setup>
+    const props = defineProps(['books'])
+</script>
+
+<template>
+        <div class="books-list">
+            <div v-for="book in books" :key="book.isbn" class="book">
+                <div class="readIt" v-if="book.isRead">
+                    <i class="fa-solid fa-eye"></i>
+                </div>
+              <div class="book-cover">
+                <img :src="book.cover" />
+      
+                <button :class="{ isRead : book.isRead }">
+                  <i class="fa-solid fa-eye"></i>
+                  <span>{{ book.isRead ? "Lido" : "Ainda não li"}}</span
+                  >
+                </button>
+              </div>
+              <div class="book-details">
+                <p class="book-author">{{book.author}}</p>
+                <h3 class="book-title">{{book.title}}</h3>
+                <p><i class="fa-solid fa-hashtag icon"></i> {{book.isbn}}</p>
+              </div>
+            </div>
+          </div>
+</template>
