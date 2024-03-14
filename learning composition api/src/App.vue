7cce164e3890fda7a99eb5fa2dd979a46bb42d66
Lipe Scheidt <luizfscheidt2@gmail.com>
2024-03-14 14:29:38 -0300

    Learning composition api with simple examples

---
+++ learning composition api/src/App.vue
@@ -0,0 +1,59 @@
+<script setup>
+
+  import { ref, reactive } from "vue"
+  import Progress from './components/Progress.vue'
+
+  let count = ref(0);
+
+
+  let courses = reactive([
+    {
+      name: "Web dev bootcamp",
+      done: true
+    },
+    {
+      name: "Vue JS 3 Completo com Composition API, Vuex & Vue Router",
+      done: true
+    },
+    {
+      name: "Git bootcamp",
+      done: false
+    },
+  ])
+
+  let newCourse = {done: false}
+
+
+  function addNewCourse() {
+    courses.push(newCourse);
+    newCourse = {done: false};
+  }
+
+  //ref exige o uso do "value" após a variável
+  function increment(){
+    count.value++
+  }
+
+</script>
+
+<template>
+  <div>
+    <p>My courses</p>
+    <ul>
+      <li v-for="course in courses" :key="course.name" >{{course.name}}</li>
+    </ul>
+  </div>
+
+  <br>
+
+  <div>
+    <input v-model="newCourse.name" type="text">
+    <button @click="addNewCourse">add course</button>
+  </div>
+  <Progress :courses="courses" />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ learning composition api/src/components/Progress.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { computed, defineProps } from "vue";
+
+const props = defineProps(['courses'])
+
+const finishedCourses = computed(() => {
+    return props.courses.filter(course => course.done).length
+})
+</script>
+
+<template>
+    <div>
+        {{ `${finishedCourses} completed courses out of ${courses.length}` }}
+
+    </div>
+</template>
